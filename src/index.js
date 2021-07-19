dc5f3a26590c17bfb013554e170c0c5071382727
Igor Desh <desh_gambler@mail.ru>
2021-07-19 18:16:40 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 
 exports.min = function min (array) {
-  return array.length > 0 ? Math.min(...array) : 0;
+  return array && array.length > 0 ? Math.min(...array) : 0;
 }
 
 exports.max = function max (array) {
-  return array.length > 0 ? Math.max(...array) : 0;
+  return array && array.length > 0 ? Math.max(...array) : 0;
 }
    
 exports.avg = function avg (array) {
-  let sum = 0;
+  if (array && array.length > 0) {
+    let sum = 0;
   for (let i = 0; i < array.length; i++) {
      sum = sum + array[i];
-  }
+    }
   return sum / array.length; 
+}
+return 0;
 }
